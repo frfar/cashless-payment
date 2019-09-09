22fb6189e70d277219361b3c37fab14a70bb38bf
Trusit <trusitshah@users.noreply.github.com>
2019-09-09 02:20:02 -0500

    encryption test

--- cashless/src/main/java/Main.java
+++ cashless/src/main/java/Main.java
@@ -1,4 +1,6 @@
 import keypad.Keypad;
+import security.AES;
+import security.utils.Utils;
 
 import java.io.IOException;
 
@@ -6,13 +8,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Cashless Payment System!");
 
-        try {
-            Keypad keypad = Keypad.getKeypadInstance();
-            while(true) {
-                System.out.println(keypad.readPassword());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        byte[] encrypted = AES.encrypt(Utils.hexStringToByte("2b7e151628aed2a6abf7158809cf4f3c"), Utils.hexStringToByte("000102030405060708090a0b0c0d0e0f"), Utils.hexStringToByte("6bc1bee22e409f96"));
+        System.out.println(new String(encrypted));
+//        try {
+//            Keypad keypad = Keypad.getKeypadInstance();
+//            while(true) {
+//                System.out.println(keypad.readPassword());
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 }
---
+++ cashless/src/main/java/security/utils/Utils.java
@@ -0,0 +1,14 @@
+package security.utils;
+
+public class Utils {
+    public static byte[] hexStringToByte(String hex) {
+        byte[] ret = new byte[hex.length()/2];
+        for (int i = 0; i < hex.length(); i++) {
+            int index = i * 2;
+            int j = Integer.parseInt(hex.substring(index, index + 2), 16);
+            ret[i] = (byte) j;
+        }
+
+        return ret;
+    }
+}

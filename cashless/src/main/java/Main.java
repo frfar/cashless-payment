14271e022cfad91fa8457831555f9af79e7ea61d
Trusit <trusitshah@users.noreply.github.com>
2019-09-09 03:19:16 -0500

    serial added

--- cashless/src/main/java/Main.java
+++ cashless/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import com.pi4j.io.serial.*;
 import keypad.Keypad;
 import security.AES;
 import security.SHA256;
@@ -10,6 +11,29 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Cashless Payment System!");
 
+        final Serial serial = SerialFactory.createInstance();
+
+        SerialConfig config = new SerialConfig();
+        config.device("/dev/" + "ttyACM0")
+                .baud(Baud._57600)
+                .dataBits(DataBits._8)
+                .parity(Parity.NONE)
+                .stopBits(StopBits._1)
+                .flowControl(FlowControl.NONE);
+
+        try {
+            serial.open(config);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        serial.addListener((SerialDataEventListener) event -> {
+            try {
+                System.out.println(event.getAsciiString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
         byte[] encrypted = AES.encrypt(Utils.hexStringToByte("2b7e151628aed2a6abf7158809cf4f3c"), Utils.hexStringToByte("000102030405060708090a0b0c0d0e0f"), Utils.hexStringToByte("6bc1bee22e409f96"));
 
         for(int i = 0; i < encrypted.length; i++) {

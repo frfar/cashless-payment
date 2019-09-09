c6351f67ac171cb7973f52c4eb62e9d9dfd6388f
Trusit <trusitshah@users.noreply.github.com>
2019-09-09 02:23:29 -0500

    bug fixed

--- cashless/src/main/java/security/utils/Utils.java
+++ cashless/src/main/java/security/utils/Utils.java
@@ -3,7 +3,7 @@ package security.utils;
 public class Utils {
     public static byte[] hexStringToByte(String hex) {
         byte[] ret = new byte[hex.length()/2];
-        for (int i = 0; i < hex.length(); i++) {
+        for (int i = 0; i < hex.length()/2; i++) {
             int index = i * 2;
             int j = Integer.parseInt(hex.substring(index, index + 2), 16);
             ret[i] = (byte) j;

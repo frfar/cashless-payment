b90be5f5a69488304433e5100d031e70f18fa096
Trusit <trusitshah@users.noreply.github.com>
2019-09-08 19:58:53 -0500

    gradle project changed

--- cashless/src/main/java/Main.java
+++ cashless/src/main/java/Main.java
@@ -1,15 +1,18 @@
+import keypad.Keypad;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Cashless Payment System!");
 
-        Converter test = new Converter();
-        String testStream = "001:006:000:STREAM 1567721034.695277" +
-                "00 00 60 00 00 00 00 00";
-        String out = test.parse(testStream);
-        int output = test.convert(out);
-        System.out.println(output);
-
-
+        try {
+            Keypad keypad = Keypad.getKeypadInstance();
+            while(true) {
+                System.out.println(keypad.readKeyPressed());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
 }
---
+++ cashless/src/main/java/keypad/Keypad.java
@@ -0,0 +1,49 @@
+package keypad;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Keypad {
+
+    private static Keypad keypad;
+
+    private static ProcessBuilder pb;
+    private static Process p;
+    private static BufferedReader br;
+
+    private static final String[] command = new String[] {"sudo","usbhid-dump","-m","04d9:1203","-es"};
+    private static final int numberOfLinePerKeyStroke = 6;
+    private static final int lineNumberWithKeyStroke = 1;
+
+    private Keypad() {
+        pb = new ProcessBuilder(Arrays.asList(command));
+    }
+
+    public static Keypad getKeypadInstance() throws IOException {
+
+        if(keypad == null || p == null || br == null) {
+            keypad = new Keypad();
+            p = pb.start();
+            br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        }
+        return keypad;
+    }
+
+    private static String readLine() throws IOException {
+        return br.readLine();
+    }
+
+    public String readKeyPressed() throws IOException {
+        for(int i = 0; i < numberOfLinePerKeyStroke; i++) {
+            String line = br.readLine();
+
+            if(i == lineNumberWithKeyStroke) {
+                return line;
+            }
+        }
+        return "";
+    }
+
+}

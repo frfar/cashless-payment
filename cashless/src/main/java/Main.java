771c0452a672bfb46931a056cf555de2f24ac99c
Trusit <trusitshah@users.noreply.github.com>
2019-09-18 16:32:47 -0500

    minor refactor

--- cashless/src/main/java/Main.java
+++ cashless/src/main/java/Main.java
@@ -35,30 +35,6 @@ public class Main {
             e.printStackTrace();
         }
 
-//        String id = "8223372036851235807";
-//        String password = "1991";
-//        byte[] passwordBytes = password.getBytes();
-//
-//        byte[] hmac = SHA256.getHMAC(id, passwordBytes);
-//        String encodedHmac = new String(Base64.getEncoder().encode(hmac));
-//
-//        byte[] encryptedMoney = AES.encrypt(Utils.hexStringToBytes("2b7e151628aed2a6abf7158809cf4f3c"), Utils.hexStringToBytes("000102030405060708090a0b0c0d0e0f"), Utils.hexStringToBytes("10"));
-//        String encodedMoney = new String(Base64.getEncoder().encode(encryptedMoney));
-//        System.out.println(id + " " + encodedHmac + " " + encodedMoney);
-//        try {
-//            serial.write(id + " " + encodedHmac + " " + encodedMoney +"\r\n");
-//            Thread.sleep(1000);
-//        } catch (IOException | InterruptedException e) {
-//            e.printStackTrace();
-//        }
-
-        serial.addListener(event -> {
-            try {
-                System.out.println(event.getAsciiString().trim());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
         serial.addListener(event -> {
             try {
                 String cardString = event.getAsciiString().trim();
@@ -118,16 +94,7 @@ public class Main {
                             } else {
                                 System.out.println("You spent: $" + totalPrice);
                                 System.out.println("You have $" + newMoney + " left in your card");
-                                byte[] encryptNewMoney = AES.encrypt(Utils.hexStringToBytes("2b7e151628aed2a6abf7158809cf4f3c"), Utils.hexStringToBytes("000102030405060708090a0b0c0d0e0f"),new byte[]{(byte)newMoney});
-                                String encodedNewMoney = Base64.getEncoder().encodeToString(encryptNewMoney);
-
-                                String newCardString = cardStrings[0] + " " + cardStrings[1] + " " + encodedNewMoney;
-
-                                serial.write(newCardString + "\r\n");
-
-                                Thread.sleep(1000);
-
-                                System.out.println("Transaction successfully finished");
+                                writeBackMoney(serial, cardStrings, (byte) newMoney);
                             }
                         }
                     } else if (args[0].equals("atm")) {
@@ -153,16 +120,7 @@ public class Main {
                             } else {
                                 System.out.println("You added: $" + selectedMoney);
                                 System.out.println("You now have $" + newMoney + " in your card");
-                                byte[] encryptNewMoney = AES.encrypt(Utils.hexStringToBytes("2b7e151628aed2a6abf7158809cf4f3c"), Utils.hexStringToBytes("000102030405060708090a0b0c0d0e0f"),new byte[]{(byte)newMoney});
-                                String encodedNewMoney = Base64.getEncoder().encodeToString(encryptNewMoney);
-
-                                String newCardString = cardStrings[0] + " " + cardStrings[1] + " " + encodedNewMoney;
-
-                                serial.write(newCardString + "\r\n");
-
-                                Thread.sleep(1000);
-
-                                System.out.println("Transaction successfully finished");
+                                writeBackMoney(serial, cardStrings, (byte) newMoney);
                             }
                         }
                     }
@@ -172,46 +130,19 @@ public class Main {
             }
         });
 
-//        serial.addListener((SerialDataEventListener) event -> {
-//            try {
-//                String cardString = event.getAsciiString().trim();
-//                System.out.println("Messaged Received from Card:");
-//                System.out.print(event.getAsciiString());
-//
-//                byte[] encryptedMoney = AES.encrypt(Utils.hexStringToBytes("2b7e151628aed2a6abf7158809cf4f3c"), Utils.hexStringToBytes("000102030405060708090a0b0c0d0e0f"), Utils.hexStringToBytes("6bc1bee22e409f96"));
-//
-//                System.out.println("Please Enter your 4 digit pin:");
-//
-//                Keypad keypad = Keypad.getKeypadInstance();
-//                String password = keypad.readPassword();
-//                byte[] passwordBytes = password.getBytes();
-//                System.out.println("You have entered: " + password);
-//
-//                byte[] hmac = SHA256.getHMAC(encryptedMoney, passwordBytes);
-//
-//                String encryptedString = new String(Base64.getEncoder().encode(hmac));
-//                System.out.println("The encryptedMoney String is:" + encryptedString);
-//
-//                if(!encryptedString.equals(cardString)) {
-//                    System.out.println("You have entered incorrect pin!!");
-//                } else {
-//                    System.out.println("You have entered correct pin!!");
-//                }
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        });
-
-//        Keypad keypad = null;
-//        try {
-//            keypad = Keypad.getKeypadInstance();
-//            String line = keypad.readLine();
-//
-//            System.out.println(line);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
         while (true);
+    }
+
+    private static void writeBackMoney(Serial serial, String[] cardStrings, byte newMoney) throws IOException, InterruptedException {
+        byte[] encryptNewMoney = AES.encrypt(Utils.hexStringToBytes("2b7e151628aed2a6abf7158809cf4f3c"), Utils.hexStringToBytes("000102030405060708090a0b0c0d0e0f"),new byte[]{newMoney});
+        String encodedNewMoney = Base64.getEncoder().encodeToString(encryptNewMoney);
+
+        String newCardString = cardStrings[0] + " " + cardStrings[1] + " " + encodedNewMoney;
+
+        serial.write(newCardString + "\r\n");
+
+        Thread.sleep(1000);
+
+        System.out.println("Transaction successfully finished");
     }
 }

d716a25e7b87603ecf854cc8d65ad76fde04bb2e
Tung Vu <tungld98vn@gmail.com>
2019-10-27 22:50:50 -0500

    SequelizeConnection

---
+++ nodejs/SequelizeConnection.js
@@ -0,0 +1,35 @@
+module.exports = function() {
+
+    let Sequelize = require("sequelize");
+    let config = require("./database.env.local");
+
+    var sequelize = null;
+
+    function initConnection() {
+        if(sequelize == null || sequelize.isConnected){
+            try {
+                console.log("user: " + config.username + "   Pass: " + config.password);
+                sequelize = new Sequelize(config.dbname, config.username, config.password, {
+                    host: config.host,
+                    dialect: 'mysql',
+                    port: 3306,
+                    define: {
+                        timestamps: false
+                    }
+                });
+            }catch(err){
+                throw err;
+            }
+        }
+    }
+
+    return {
+        sequelize: function getSequalize() {
+            if(sequelize == null) {
+                initConnection();
+            }
+
+            return sequelize;
+        }
+    }
+}();
